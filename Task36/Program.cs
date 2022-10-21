0242c23cd40b58feb5972c334d0494e933c92657
Gregan <343314@gmail.com>
2022-10-21 14:44:51 +0300

    Task36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,36 +1,30 @@
 ﻿// Задание №36
 
 Console.Clear();
-Console.WriteLine("Введите размер массива");
 
-    int size = Convert.ToInt32(Console.ReadLine());
-    int[] numbers = new int[size];
+Console.Write($"Введи количество элементов массива: ");
 
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
 
-    int sum = 0;
+int RandomNumbers(int numberElements, int min, int max)
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
 
-for (int z = 0; z < numbers.Length; z+=2)
-    sum = sum + numbers[z];
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
 
-    Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
+      Console.Write(randomNumbers[i] + " ");
 
-void FillArrayRandomNumbers(int[] numbers)
-{
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            numbers[i] = new Random().Next(1,10);
-        }
-}
-void PrintArray(int[] numbers)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
-        {
-            Console.Write(numbers[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
+    }
+  return sumElements;
+  }
+
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
